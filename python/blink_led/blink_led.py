c3cb3bdbd537b6576205879d06d7cc9b0b125513
Demo User <debian@beaglebone.localdomain>
2023-10-02 03:31:32 +0000

    Updates for HW4

---
+++ python/blink_led/blink_led.py
@@ -0,0 +1,13 @@
+import Adafruit_BBIO.GPIO as GPIO
+import time
+
+GPIO.setup("USR3",GPIO.OUT)
+
+while True:
+    GPIO.output("USR3",GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output("USR3", GPIO.LOW)
+    time.sleep(0.2)
+
+
+
